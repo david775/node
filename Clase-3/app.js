095bca2d41900b2cb829e23c7f6745fb9d636926
hector <hsdsegura6@gmail.com>
2024-02-11 23:36:01 -0500

    post patch

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express') // require -> commonJS
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by')
 
 // Todos los recursos que sean MOVIES se identifican con /movies
@@ -17,7 +20,7 @@ app.get('/movies', (req, res) => {
 })
 
 // El :id se le llama segmento dinámico app.get('/movies/:id/:mas/:otro', (req, res) => {
-app.get('/movies/:id', (req, res) => { // se puede usar regex, path-to-regex  es una libreria
+app.get('/movies/:id', (req, res) => { // se puede usar regex, path-to-regex  es una librería
   const { id } = req.params
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
@@ -25,8 +28,53 @@ app.get('/movies/:id', (req, res) => { // se puede usar regex, path-to-regex  es
   res.status(404).json({ message: 'Movie not found' })
 })
 
-const PORT = process.env.port ?? '1234'
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
 
+  if (result.error) {
+    // 422 Unprocessable entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    Id: crypto.randomUUID(), // uuid v4
+    ...result
+  }
+
+  // Esto no seria REST porque se guarda
+  // el estado de la aplicación en la memoria
+  movies.push(newMovie)
+
+  res.status(200).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  if (!result.success) {
+    // 422 Unprocessable entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  // const movie = movies.find(movie => movie.id === id)
+  // if (!movie) return res.status(404).json({ message: 'Movie not found' })
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+  console.log(movies[movieIndex] + '1 ', result.data)
+  console.log(updateMovie)
+
+  movies[movieIndex] = updateMovie
+
+  return res.status(200).json(updateMovie)
+})
+// })
+
+const PORT = process.env.port ?? '1234'
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`)
 })
