78b3a1562220ec6cd21ea60fa308f0abe992a735
hector <hsdsegura6@gmail.com>
2024-09-15 10:16:29 -0500

    b

---
+++ user-auth/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { PORT } from './config.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
+app.post('/login', (req, res) => { })
+app.post('/register', (req, res) => { })
+app.post('/logout', (req, res) => { })
+
+app.get('/protected', (req, res) => { })
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
